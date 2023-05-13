28f1c0f0c39012aadca83d0baadd186f2a94110d
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2023-05-13 12:17:00 -0300

    Cambio de estilo de hero image y agregado de footer

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const boton = document.getElementById("button-tema")
+const botonDeTema = document.getElementById("button-tema")
 const htlm = document.querySelector("html")
 
-boton.addEventListener('click', () => {
+botonDeTema.addEventListener('click', () => {
     htlm.classList.toggle('dark')
+})
+
+const typed = new Typed('.typed', {
+    strings: ['amiga', 'compañera', 'familia'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 2000,
+    cursorChar: '_'
+})
+
+const botonCambioDeSeccion = document.getElementById('cambio-seccion')
+const coordenadasDeSeccion = botonCambioDeSeccion.getBoundingClientRect()
+const coordenadaMediaY = (coordenadasDeSeccion.bottom + coordenadasDeSeccion.top) / 2
+
+botonCambioDeSeccion.addEventListener('click', () => {
+    window.scrollTo({
+        top: coordenadaMediaY,
+        behavior: 'smooth'
+    })
 })
